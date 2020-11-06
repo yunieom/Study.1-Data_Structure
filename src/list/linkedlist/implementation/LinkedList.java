994413311256996f505cc698564e5b7fe121f15b
yunieom <yunzoo0915@gmail.com>
2020-11-06 23:49:03 +0900

    Nov 6, 2020
    생활코딩 자료구조 30/49

--- src/list/linkedlist/implementation/LinkedList.java
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -19,6 +19,55 @@ public class LinkedList {
 		}
 		
 	}
+	//addFirst 구현. head에 data넣기 
+	public void addFirst(Object input) {
+		Node newNode = new Node(input); // line 7의 Node 클래스가 객체화 될 것이고 그 클래스의 생성자인 node 메소드 (line 12)에 input이 들어갈 것이다.
+		//위 코드 실행하고 나면 첫 노드의 데이터는 30. (Main.java 의 line 6 에서 30을 input 값으로 넣어줬다. next는 null.
+		newNode.next = head; // 새로만들어진 노드의 넥스트 값으로 head값 지정
+		head = newNode; // head값이 새로만들어진 노드값
+		size++; // 새로만들어진 노드를 가르키고 있으므로 총 들어있는 값이 2개. 그러므로 사이즈를 1증가 시켜준다.
+		
+		if(head.next == null) { // head의 next가 null이라면 = 값이 존재하지 않는다면,
+			tail = head; // 마지막 노드(tail)는 head와 같다.
+		}
+	}
+	//addLast 구현. tail에 data넣기
+	public void addLast(Object input) {
+		Node newNode = new Node(input);
+		if(size == 0) { //list에 data가 없을 경우 헤드에 넣나 테일에 넣나 무의미함 그렇기 때문에
+			addFirst(input); //addFirst 메소드로 data 추가. data가 없을때 else 함수를 타면 tail.next(line 40)가 말이 안됨.
+		} else { //size 가 0보다 큰 경우: list에 data가 있는 경우 
+			tail.next = newNode; //새로 추가한 데이터를 인식시켜주기 tail의 next로 인식 시키고
+			tail = newNode; // 바로 윗줄에서 언급한 newNode를 다시 tail로 인식 시킨다.
+			size++; // 새로 데이터를 추가했으므로 사이즈 1 증가 
+		}
+	}
+	//인덱스별로 data 값을 보기위한 Node 구현
+	Node node(int index) {
+		Node x = head;
+		for(int i=0; i<index; i++) {
+			x = x.next;
+		}
+		return x;
+	}
+	//add 구현
+	public void add(int k, Object input) {
+		if(k == 0) { //0번째 인덱스에 data를 넣는 경우 = list에 data가 없을 경우 
+			addFirst(input); // addFirst 구현
+		} else { // 0번째 인덱스가 아닌 다른 인덱스자리에 data를 넣는 경우 k번째 -1 인덱스의 노드값을 알아내서 k번째 인덱스의 노드값을 알아서 k+1 한다.
+			Node temp1 = node(k-1); // k-1 번째 노드를 temp1에 담고 
+			Node temp2 = temp1.next; // k번째 노드를 temp2에 담고
+			Node newNode = new Node(input); // 추가할 노드인 newNode 생성
+			temp1.next = newNode; //temp1의 다음 노드는 추가할 노드인 newNode
+			newNode.next = temp2; // newNode의 다음 노드는 기존의 k번째 노드의 값. 즉 (k+1이 된다)
+			size++;
+			if(newNode.next == null) { //만약 newNode 다음 값이 null이라면 = 값이 존재하지 않는다면
+				tail = newNode; // 마지막 노드(tail)는 newNode이다. k+1로 기존값을 미뤄줄 필요가 없음. 즉 addLast의 경우가 되는 것.
+			}
+		}
+		
+	}
+	
 
 	
 }
--- src/list/linkedlist/implementation/Main.java
+++ src/list/linkedlist/implementation/Main.java
@@ -3,6 +3,20 @@ package list.linkedlist.implementation;
 public class Main {
 	public static void main(String[] args) {
         LinkedList numbers = new LinkedList(); //인스턴스화
+        numbers.addFirst(30);
+        numbers.addFirst(20);
+        numbers.addFirst(10);
+        System.out.println(numbers.node(0));// 맨 마지막으로(제일최근) addFirst가 실행된 10이 호출
+        System.out.println(numbers.node(1));// 두번째 마지막으로 addFirst가 실행된 20이 호출
+        System.out.println(numbers.node(2));// 세번째 마지막으로 addFirst가 실행된 30이 호출
         
+        numbers.addLast(40);        
+        numbers.addLast(50);        
+        numbers.addLast(60);   
+        System.out.println(numbers.node(3)); // 제일 처음 addLast가 실행된 40이 호출
+        System.out.println(numbers.node(4)); // 두번째 처음 addLast가 실행된 50이 호출
+        System.out.println(numbers.node(5)); // 세번째 처음 addLast가 실행된 60이 호출
+        
+        numbers.add(1, 15); // 1번 인덱스에 값 15 을 넣겠다.
     }
 }
